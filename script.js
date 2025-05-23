107e37b75ffe2b1a9f1c87284ffee7eff383c037
ivan <141366255+navisotas@users.noreply.github.com>
2025-05-23 11:19:47 -0300

    sddsd

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+consr pergunta =[
+ {
+    anunciado: "pergunta1"
+    alternativas: [alternava1,"alternativa2"]
+ }
+ {
+    anunciado: "pergunta2"
+    alternativas: [alternava1,"alternativa2"]
+ }
